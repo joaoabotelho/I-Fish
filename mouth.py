4834d8e271ab83bbe8cc8aef7dd11dfced8795e3
Tiago Manuel Dias Martins <tmmartins@student.dei.uc.pt>
2018-04-22 08:30:38 +0100

    first test

--- mouth.py
+++ mouth.py
@@ -11,7 +11,7 @@ from threading import Thread
 from audio_analytics import AudioInformation
 import RPi.GPIO as GPIO
 
-FILE_NAME = './response.wav'
+FILE_NAME = './tests/response.wav'
 CLIENT_ACCESS_TOKEN = 'f0edfee5f0964102aac241ce5f13200b'
 speechRec = google1.speechRecognition(CLIENT_ACCESS_TOKEN)
 
@@ -47,14 +47,14 @@ def main():
 
             i = len(norm)-1
             D2A.ChangeDutyCycle(0)
-            audio = speechRec.record()
-            if speechRec.analyze(audio):
-                test = AudioInformation(FILE_NAME)
-                norm = test.normalized
-                durations = test.array_of_time
-                t_start_animation = time.time() # in seconds ---------x.x
-                pygame.mixer.music.load(FILE_NAME)
-                pygame.mixer.music.play(0)
+            # audio = speechRec.record()
+            # if speechRec.analyze(audio):
+            test = AudioInformation(FILE_NAME)
+            norm = test.normalized
+            durations = test.array_of_time
+            t_start_animation = time.time() # in seconds ---------x.x
+            pygame.mixer.music.load(FILE_NAME)
+            pygame.mixer.music.play(0)
             i = 0
 
 if __name__ == "__main__":

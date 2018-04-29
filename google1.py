a3bd3514f952de625b997fd48da37a4b4f2d3d94
João Botelho <joao.botelho@jeknowledge.com>
2018-04-29 17:44:56 +0100

    divides google1.py per classes

--- google1.py
+++ google1.py
@@ -8,14 +8,6 @@ from gtts import gTTS
 import os
 from tempfile import TemporaryFile
 
-try:
-    import apiai
-except ImportError:
-    sys.path.append(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
-    )
-    import apiai
-
 CLIENT_ACCESS_TOKEN = 'f0edfee5f0964102aac241ce5f13200b'
 MAX_INT = 1000000000
 
@@ -70,26 +62,6 @@ class speechRecognition:
                 return os.path.join(root, name)
         return False
 
-class googleApiRequest:
-
-    def __init__(self, client_access_token):
-        self.client_access_token = client_access_token
-        self.ai = apiai.ApiAI(self.client_access_token)
-        self.session_id = randint(1, MAX_INT)
-
-    def expectResponse(self, query):
-        request = self.ai.text_request()
-        request.query = query
-        request.session_id = self.session_id
-        response = request.getresponse()
-        parsed = json.loads(response.read().decode())
-        print(json.dumps(parsed, indent=4))
-        text_response = parsed['result']['fulfillment']['speech']
-        score = parsed['result']['score']
-        if score == 0.0:
-            text_response = 'I didn\'t understand'
-        print('Response: ', text_response)
-        return text_response
 
 if __name__ == '__main__':
     speechRec = speechRecognition(CLIENT_ACCESS_TOKEN)
---
+++ google_api_request.py
@@ -0,0 +1,35 @@
+from random import randint
+import os.path
+import json
+import sys
+
+try:
+    import apiai
+except ImportError:
+    sys.path.append(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+    )
+    import apiai
+
+MAX_INT = 1000000000
+
+class googleApiRequest:
+
+    def __init__(self, client_access_token):
+        self.client_access_token = client_access_token
+        self.ai = apiai.ApiAI(self.client_access_token)
+        self.session_id = randint(1, MAX_INT)
+
+    def expectResponse(self, query):
+        request = self.ai.text_request()
+        request.query = query
+        request.session_id = self.session_id
+        response = request.getresponse()
+        parsed = json.loads(response.read().decode())
+        print(json.dumps(parsed, indent=4))
+        text_response = parsed['result']['fulfillment']['speech']
+        score = parsed['result']['score']
+        if score == 0.0:
+            text_response = 'I didn\'t understand'
+        print('Response: ', text_response)
+        return text_response

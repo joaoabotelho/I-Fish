c99799d1e730cdac29d79fc812b1ddb99da479a5
João Botelho <joao.botelho@jeknowledge.com>
2018-04-30 23:36:04 +0100

    correction of read files

--- music/play_song.py
+++ music/play_song.py
@@ -3,21 +3,22 @@ import pygame
 import time
 import numpy as np
 
-def read_from_file(path):
+def read_from_file(path, norm=False):
     f = open(path, 'r')
     a = np.array([])
     for line in f:
         numb = float(line)
-        if(numb <= 0.2):
-            numb = 0
-        elif(numb > 0.2 and numb <= 0.4):
-            numb = 0.3
-        elif(numb > 0.4 and numb <= 0.6):
-            numb = 0.5
-        elif(numb > 0.6 and numb <= 0.8):
-            numb = 0.7
-        elif(numb > 0.8 and numb < 1):
-            numb = 0.9
+        if norm == True:
+            if(numb <= 0.2):
+                numb = 0
+            elif(numb > 0.2 and numb <= 0.4):
+                numb = 0.3
+            elif(numb > 0.4 and numb <= 0.6):
+                numb = 0.5
+            elif(numb > 0.6 and numb <= 0.8):
+                numb = 0.7
+            elif(numb > 0.8 and numb < 1):
+                numb = 0.9
         a = np.append(a, numb)
     f.close()
     return a
@@ -33,7 +34,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 
-norm = read_from_file(NORM_MUSIC_FILE)
+norm = read_from_file(NORM_MUSIC_FILE, norm=True)
 durations = read_from_file(DURAT_MUSIC_FILE)
 
 D2A = GPIO.PWM(25,100)
@@ -46,7 +47,6 @@ pygame.mixer.music.play(0)
 
 for x,y in zip(norm, durations):
     D2A.ChangeDutyCycle(x*100)
-    print (x)
     t_end_animation = time.time()
     animation_time = t_end_animation - t_start_animation
 

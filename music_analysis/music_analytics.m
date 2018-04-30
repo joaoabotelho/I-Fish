7a32c13f5efbac4d43ac664dd2d5027e4eeb0ee4
João Botelho <joao.botelho@jeknowledge.com>
2018-04-30 20:19:08 +0100

    delete dir

--- music_analysis/music_analytics.m
+++
@@ -1,8 +0,0 @@
-[y,fs] = wavread(filename); 
-
-y = y(:,1);
-dt = 1/fs;
-t = 0:dt:(length(y)*dt)-dt;
-plot(t,y); xlabel('Seconds'); ylabel('Amplitude');
-figure
-plot(psd(spectrum.periodogram,y,'Fs',fs,'NFFT',length(y)));
